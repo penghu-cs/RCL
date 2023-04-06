d747355df24aaaa6431b7133ab603c12cd9a6b74
penghu-cs <45786915+penghu-cs@users.noreply.github.com>
2023-04-07 02:15:07 +0800

    Update train_f30k.sh

--- RCL-SGRAF/train_f30k.sh
+++ RCL-SGRAF/train_f30k.sh
@@ -7,13 +7,13 @@ margin=0.2
 data_path=../data
 vocab_path=../data/vocab
 gpu=0
+lr_update=15
+num_epochs=30
 
 model_name=runs/$data_name/checkpoint
 logger_name=runs/$data_name/log
 
 module_name=SGR
-lr_update=30
-num_epochs=40
 CUDA_VISIBLE_DEVICES=$gpu python train.py --data_name $data_name --num_epochs $num_epochs --loss $loss --lr_update $lr_update --module_name $module_name --log_step 200 --data_path $data_path --vocab_path $vocab_path --model_name $model_name --logger_name $logger_name --noise_rate $noise_rate --margin $margin --tau $tau --batch_size $batch_size
 
 # evaluate SGR
@@ -21,8 +21,6 @@ CUDA_VISIBLE_DEVICES=$gpu python train.py --data_name $data_name --num_epochs $n
 # CUDA_VISIBLE_DEVICES=$gpu python evaluate_model.py --data_name $data_name --num_epochs $num_epochs --loss $loss --lr_update $lr_update --module_name $module_name --log_step 200 --data_path $data_path --vocab_path $vocab_path --model_name $model_name --logger_name $logger_name --noise_rate $noise_rate --margin $margin --tau $tau --batch_size $batch_size
 
 module_name=SAF
-lr_update=20
-num_epochs=30
 CUDA_VISIBLE_DEVICES=$gpu python train.py --data_name $data_name --num_epochs $num_epochs --loss $loss --lr_update $lr_update --module_name $module_name --log_step 200 --data_path ./data --vocab_path ./vocab --model_name $model_name --logger_name $logger_name --noise_rate $noise_rate --margin $margin --tau $tau --batch_size $batch_size
 
 # evaluate SAF
